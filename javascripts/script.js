d40cf914fd1cbf9924edb0f694038f35aff40697
Luke Atherton <latherton@mcaus.com.au>
2012-12-07 15:25:41 +1000

    Cleaned up console scripts.

--- javascripts/script.js
+++ javascripts/script.js
@@ -9,43 +9,25 @@ xmlDoc = $.parseXML(filesys),
 $xml = $( xmlDoc ),
 $title = 'edgeNet';
 
-function updLstChr(){ // blinking cursor
-		var cont=this.content(); // get console 
-		if(cont.substring(cont.length-1,cont.length)=="|") // if last char is the cursor
-			$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it
-		else
-			this.write("|"); // else write it
-	}
+function openSidebar(cameraSource) {
 
-//$(function() {
-        // run the currently selected effect
-function runEffect(cameraSource) {
-        	//$('#cameraFrame').hide();
-        	$('#loading').show();
+   	$('#loading').show();
 
-            // get effect type from
-            var selectedEffect = 'slide';
+	// get effect type from
+	var selectedEffect = 'slide';
  
-            // most effect types need no options passed by default
-            var options = {};
-            // some effects have required parameters
-            if ( selectedEffect === "scale" ) {
-                options = { percent: 100 };
-            } else if ( selectedEffect === "size" ) {
-                options = { to: { width: 280, height: 185 } };
-            }
+	// most effect types need no options passed by default
+	var options = {};
 
-            options = {direction: "right"}
+	options = {direction: "right"}
  
-            // run the effect
-            $( "#content" ).show( selectedEffect, options, 800, callback );
+	// run the effect
+	$( "#content" ).show( selectedEffect, options, 800, callback );
 
-            $('#cameraFrame').attr('src', cameraSource);
-				$('#cameraFrame').load(function() 
-			    {
-			    	$('#loading').hide();
-			        //$('#cameraFrame').show();
-			    });
+	$('#cameraFrame').attr('src', cameraSource);
+		$('#cameraFrame').load(function(){
+			$('#loading').hide();
+	});
 };
 
 function closeSidebar() {
@@ -59,87 +41,36 @@ function closeSidebar() {
             $( "#content" ).hide( selectedEffect, options, 800, callback );
 };
  
-        //callback function to bring a hidden box back
+//Callback for close sidebar functions
 function callback() {
-            /*setTimeout(function() {
-                $( "#frame:visible" ).removeAttr( "style" ).fadeOut();
-            }, 1000 );*/
+
 };
- 
-        
-   // });
 
 $(document).ready(function () {
 
 	// check where the content-div is  
  	var offset = $('#content').offset();  
 
-	 $(window).scroll(function () {  
-	   var scrollTop = $(window).scrollTop(); // check the visible top of the browser  
+	$(window).scroll(function () {  
+	   	var scrollTop = $(window).scrollTop(); // check the visible top of the browser  
 
-	   if (offset.top<scrollTop) $('#content').addClass('fixed');  
-	   else $('#content').removeClass('fixed');  
-	  });
+	   	if (offset.top<scrollTop){
+			$('#content').addClass('fixed');
+	   	} else {
+	   		$('#content').removeClass('fixed');  
+	   	}
+	 });
 
 	$( "#content" ).hide();
 
 	var accessCountimer = null;
-	accessCountimer=setInterval(function(){Typer.updLstChr();},500);
+	accessCountimer=setInterval(function(){updLstChr();},500);
 
-
-
-		 /*$.ajax({
-		   url: "http://teethtracker.heroku.com/device_movements.json",
-		   dataType: "jsonp",
-		   success: function(data){
-			alert("success");
-			 
-			 // Returned data available in object "xml"
-			 $("body").append("<div id='output' style='margin-left:1em; color:#FF6600;'>test</div>");
-		   },
-			error: function(data) {
-				//alert("error");
-				var test = "";
-				$.each(data, function (key, val) {
-					test += key + "-" + val + "</br>";
-                // Format the text to display.
-                //var str = val.WorkOrderID + '-' + val.Reason;
-                //alert(str); 
-
-                // Add a list item for the product.
-                //$('<li/>', { html: str })
-                //.appendTo($('.body'));
-				});
-				//$("body").append("<div id='output' style='margin-left:1em; color:#FF6600;'>" + test + "</div>");
-			}
-		 });*/
-
-        /*$.getJSON("http://teethtracker.heroku.com/device_movements.json?callback=?",
-        function (data) {
-            alert("success");
-            //$.each(data, function (key, val) {
-
-                // Format the text to display.
-                //var str = val.WorkOrderID + '-' + val.Reason;
-                //alert(str); 
-
-                // Add a list item for the product.
-                //$('<li/>', { html: str })
-                //.appendTo($('.body'));
-            //});
-			$("body").append("<div id='output' style='margin-left:1em; color:#FF6600;'>test</div>");
-        });*/
+	$(document).keypress(function(event){ 
+		//Capture the keydown event and call the addText, this is executed on page load
+		addText( event ); 
+	});
 });
-
-$(
-	function(){
-		$( document ).keypress (
-			function ( event ) { 
-				Typer.addText( event ); //Capture the keydown event and call the addText, this is executed on page load
-			}
-		);
-	}
-);
 
 $(document).unbind('keydown').bind('keydown', function (event) {
     var doPrevent = false;
@@ -289,32 +220,32 @@ $(document).unbind('keydown').bind('keydown', function (event) {
 				switch(inputArray[0]){
 				case "camera1":
 					$xml.find(path[path.length-1]).eq(0).children(inputArray[0]).each(function() {
-						runEffect("cameraSource1.html");
+						openSidebar("cameraSource1.html");
 					});
 				  	break;
 				case "camera2":
 				  	$xml.find(path[path.length-1]).eq(0).children(inputArray[0]).each(function() {
-						runEffect("cameraSource2.html");
+						openSidebar("cameraSource2.html");
 					});
 				  	break;
 				case "camera3":
 				  	$xml.find(path[path.length-1]).eq(0).children(inputArray[0]).each(function() {
-						runEffect("cameraSource3.html");
+						openSidebar("cameraSource3.html");
 					});
 				 	break;
 				case "camera4":
 				  	$xml.find(path[path.length-1]).eq(0).children(inputArray[0]).each(function() {
-						runEffect("cameraSource4.html");
+						openSidebar("cameraSource4.html");
 					});
 				  	break;
 				case "camera5":
 				  	$xml.find(path[path.length-1]).eq(0).children(inputArray[0]).each(function() {
-						runEffect("cameraSource5.html");
+						openSidebar("cameraSource5.html");
 					});
 				  	break;
 				case "camera6":
 				  	$xml.find(path[path.length-1]).eq(0).children(inputArray[0]).each(function() {
-						runEffect("cameraSource6.html");
+						openSidebar("cameraSource6.html");
 					});
 				  	break;
 				default:
@@ -363,7 +294,6 @@ $(document).unbind('keydown').bind('keydown', function (event) {
 		case 38:
 			var elems = $(".command"); // returns a nodeList
 			var arr = jQuery.makeArray(elems);
-			//arr.reverse(); // use an Array method on list of dom elements
 			$(arr).appendTo(document.body);
 			
 			doPrevent = false;
@@ -378,107 +308,27 @@ $(document).unbind('keydown').bind('keydown', function (event) {
     }
 });
 
-var Typer={
-	text: null,
-	accessCountimer:null,
-	index:0, // current cursor position
-	speed:2, // speed of the Typer
-	file:"", //file, must be setted
-	accessCount:0, //times alt is pressed for Access Granted
-	deniedCount:0, //times caps is pressed for Access Denied
-	init: function(){// inizialize Hacker Typer
-		accessCountimer=setInterval(function(){Typer.updLstChr();},500); // inizialize timer for blinking cursor
-		/*$.get(Typer.file,function(data){// get the text file
-			Typer.text=data;// save the textfile in Typer.text
-		});*/
-	},
+//Main function to add the code	
+function addText(key){
+	var cont = $("#console").html();
 	
-	content:function(){
-		return $("#console").html();// get console content
-	},
-	
-	write:function(str){// append to console content
-		$("#console").append(str);
-		return false;
-	},
-	
-	makeAccess:function(){//create Access Granted popUp      FIXME: popup is on top of the page and doesn't show is the page is scrolled
-		Typer.hidepop(); // hide all popups
-		Typer.accessCount=0; //reset count
-		var ddiv=$("<div id='gran'>").html(""); // create new blank div and id "gran"
-		ddiv.addClass("accessGranted"); // add class to the div
-		ddiv.html("<h1>ACCESS GRANTED</h1>"); // set content of div
-		$(document.body).prepend(ddiv); // prepend div to body
-		return false;
-	},
-	makeDenied:function(){//create Access Denied popUp      FIXME: popup is on top of the page and doesn't show is the page is scrolled
-		Typer.hidepop(); // hide all popups
-		Typer.deniedCount=0; //reset count
-		var ddiv=$("<div id='deni'>").html(""); // create new blank div and id "deni"
-		ddiv.addClass("accessDenied");// add class to the div
-		ddiv.html("<h1>ACCESS DENIED</h1>");// set content of div
-		$(document.body).prepend(ddiv);// prepend div to body
-		return false;
-	},
-	
-	hidepop:function(){// remove all existing popups
-		$("#deni").remove();
-		$("#gran").remove();
-	},
-	
-	addText:function(key){//Main function to add the code
-		if(key.keyCode==18){// key 18 = alt key
-			Typer.accessCount++; //increase counter 
-			if(Typer.accessCount>=3){// if it's presed 3 times
-				Typer.makeAccess(); // make access popup
-			}
-		}else if(key.keyCode==20){// key 20 = caps lock
-			Typer.deniedCount++; // increase counter
-			if(Typer.deniedCount>=3){ // if it's pressed 3 times
-				Typer.makeDenied(); // make denied popup
-			}
-		}else if(key.keyCode==27){ // key 27 = esc key
-			Typer.hidepop(); // hide all popups
-		}/*else if(Typer.text){ // otherway if text is loaded
-			var cont=Typer.content(); // get the console content
-			if(cont.substring(cont.length-1,cont.length)=="|") // if the last char is the blinking cursor
-				$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it before adding the text
-			if(key.keyCode!=8){ // if key is not backspace
-				Typer.index+=Typer.speed;	// add to the index the speed
-			}else{
-				if(Typer.index>0) // else if index is not less than 0 
-					Typer.index-=Typer.speed;//	remove speed for deleting text
-			}
-			var text=$("<div/>").text(Typer.text.substring(0,Typer.index)).html();// parse the text for stripping html enities
-			var rtn= new RegExp("\n", "g"); // newline regex
-			var rts= new RegExp("\\s", "g"); // whitespace regex
-			var rtt= new RegExp("\\t", "g"); // tab regex
-			$("#console").html(text.replace(rtn,"<br/>").replace(rtt,"&nbsp;&nbsp;&nbsp;&nbsp;").replace(rts,"&nbsp;"));// replace newline chars with br, tabs with 4 space and blanks with an html blank
-			window.scrollBy(0,50); // scroll to make sure bottom is always visible
-		}*/
-		/*if ( key.preventDefault && key.keyCode != 122 ) { // prevent F11(fullscreen) from being blocked
-			//key.preventDefault()
-		};  
-		if(key.keyCode != 122){ // otherway prevent keys default behavior
-			//key.returnValue = false;
-		}*/
-		
-		var cont=this.content();
-		
-		if(cont.substring(cont.length-1,cont.length)=="|"){ // if last char is the cursor
-			$("#console").html($("#console").html().substring(0,cont.length-1));
-		} // remove it
-		$("#console").append(String.fromCharCode(key.keyCode));
-		
-	},
-	
-	updLstChr:function(){ // blinking cursor
-		var cont=this.content(); // get console 
-		if(cont.substring(cont.length-1,cont.length)=="|") // if last char is the cursor
-			$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it
-		else
-			this.write("|"); // else write it
+	if(cont.substring(cont.length-1,cont.length)=="|"){ // if last char is the cursor
+		$("#console").html($("#console").html().substring(0,cont.length-1));
+	} // remove it
+	write(String.fromCharCode(key.keyCode));	
+}
+
+// blinking cursor
+function updLstChr(){ 
+	var cont = $("#console").html(); // get console 
+	if(cont.substring(cont.length-1,cont.length)=="|"){ // if last char is the cursor
+		$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it
+	}else{
+		write("|"); // else write it
 	}
 }
 
-
+function write(str){// append to console content
+	$("#console").append(str);
+	return false;
+}
